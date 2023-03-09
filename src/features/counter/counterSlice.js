71c9ee16478227ca653b033aedc1a28b623c3b7d
Bernard Calma <nard.calma@gmail.com>
2023-03-08 20:41:52 -0800

    add and minus reducers added and tested

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -1,14 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    counter: 0,
+    count: 0,
 }
 
 const counterSlice = createSlice({
     name: "counter",
     initialState,
+    reducers: {
+        add: (state) => {
+            state.count += 1;
+        },
+        minus: (state) =>{
+            if (state.count > 0) state.count -= 1;
+        }
+    }
 })
 
-// console.log(counterSlice)
+console.log(counterSlice)
 
+export const {add, minus} = counterSlice.actions;
 export default counterSlice.reducer;
--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,14 +1,21 @@
 import "../styles/list.css"
 // Store
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { add, minus } from "../features/counter/counterSlice"
+
 const List = () => {
-    const {counter} = useSelector((state) => state.counter)
-    console.log(counter)
+    console.log(useSelector((store)=>console.log(store)))
+
+    const dispatch = useDispatch()
+    const {count} = useSelector((store) => store.counter)
+    // console.log(counter)
     return(
         <div>
             <h1>Your List Goes Here</h1>
             <div className="containerList">
-
+                <p>Counter: {count}</p>
+                <button onClick={()=>dispatch(add())}>+</button>
+                <button onClick={()=>dispatch(minus())}>-</button>
             </div>
         </div>
     )
