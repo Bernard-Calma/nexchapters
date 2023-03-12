e4faf2c38950833ad4279e38482805908f251e1c
Bernard Calma <nard.calma@gmail.com>
2023-03-12 14:13:53 -0700

    add navigation on add new manga

--- src/components/NewManga.jsx
+++ src/components/NewManga.jsx
@@ -15,10 +15,21 @@ const NewManga = (props) => {
     const imageStandBy = (event) => {
         event.target.src = 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg'
     }
+    const hideAddForm = () => {
+        document.querySelector(".containerAddManga").style.display = "none";
+    }
+    const showAddForm = () => {
+        document.querySelector(".containerAddManga").style.display = "flex";
+    }
+
     return(
         <div className="containerNewManga">
-            <i className="fi fi-rr-add btnAddNewManga"></i>
+            <i className="fi fi-rr-add btnAddNewManga" onClick={showAddForm}></i>
             <div className="containerAddManga">
+                <div className="formNavTop">
+                    <p className="addTitle">Add New</p>
+                    <i class="fi fi-rr-rectangle-xmark" onClick={hideAddForm}></i>
+                </div>
                 <img src="" alt="new manga" onError={imageStandBy} className="formImageNewManga"/>
                 <form>
                     <label htmlFor="animeList" className="animeList">Anime List <select name="animeList"> 
@@ -32,6 +43,10 @@ const NewManga = (props) => {
                     <input type="number" name="totalChapters" placeholder="total chapters"/>
                     <input type="number" name="currentChapter" placeholder="current chapter"/>
                 </form>
+                <div className="formNav">  
+                        <a className="cancel" onClick={hideAddForm}>cancel</a>
+                        <button className="submit">Submit</button>
+                </div>
             </div>
         </div>
     )
