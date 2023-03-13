1b34ca529e528b4d5007b5321a689e8bc1eeb395
Bernard Calma <nard.calma@gmail.com>
2023-03-12 19:53:19 -0700

    api call created and tested from jkina

--- src/components/NewManga.jsx
+++ src/components/NewManga.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const NewManga = (props) => {
     const [newManga, setNewManga] = useState({
@@ -10,7 +10,7 @@ const NewManga = (props) => {
     })
 
     // List from mapping mangalist and grabing title
-    const [mangaList] = useState(props.mangaList.map(manga => manga.title))
+    const [mangaList, setMangaList] = useState(props.mangaList)
 
     const imageStandBy = (event) => {
         event.target.src = 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg'
@@ -22,19 +22,50 @@ const NewManga = (props) => {
         document.querySelector(".containerAddManga").style.display = "flex";
     }
 
+    // Manga API call
+    const url = 'https://api.jikan.moe/v4/manga';
+    let queryTitle = "naruto"
+    let query = `?q=${queryTitle}?order_by="rank`;
+
+    const getAnime = () => {
+        fetch(url + query)
+        .then(res => res.json())
+        .then(mangaList => 
+            // console.log(mangaList.data.map(
+            //     manga => {return {
+            //         'totalChapters': manga.chapters,
+            //         'titles': manga.titles[0],
+            //         'image': manga.images.webp.image_url
+            //     }}))
+            setMangaList(mangaList.data.map(
+                    manga => {return {
+                        'totalChapters': manga.chapters,
+                        'title': manga.titles[0].title,
+                        'image': manga.images.webp.image_url
+                    }}))
+            )
+    }
+
+    useEffect(() => {
+        getAnime()
+        console.log(mangaList)
+    },[])
+    
+
+
     return(
         <div className="containerNewManga">
             <i className="fi fi-rr-add btnAddNewManga" onClick={showAddForm}></i>
             <div className="containerAddManga">
                 <div className="formNavTop">
                     <p className="addTitle">Add New</p>
-                    <i class="fi fi-rr-rectangle-xmark" onClick={hideAddForm}></i>
+                    <i className="fi fi-rr-rectangle-xmark" onClick={hideAddForm}></i>
                 </div>
                 <img src="" alt="new manga" onError={imageStandBy} className="formImageNewManga"/>
                 <form>
                     <label htmlFor="animeList" className="animeList">Anime List <select name="animeList"> 
                         {
-                            mangaList.map(manga => <option value={manga}>{manga}</option>)
+                            mangaList.map(manga => <option value={manga.title}>{manga.title}</option>)
                         }
                     </select></label>
                     <input type="text" name="title" placeholder="title"/>
