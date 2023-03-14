259d770fd8f0210b2ae93ade924848af58b66a69
Bernard Calma <nard.calma@gmail.com>
2023-03-13 23:35:31 -0700

    added home paragraphs

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -9,14 +9,14 @@ const Home = () => {
     const {count} = useSelector((store) => store.counter)
     // console.log(counter)
     return(
-        <div>
+        <section>
             <h1 className="title">Home</h1>
-            <div className="containerList">
-                <p>Counter: {count}</p>
-                <button onClick={()=>dispatch(add())}>+</button>
-                <button onClick={()=>dispatch(minus())}>-</button>
+            <div className="containerHome">
+                <p>Do you read a lot of manga and you lost track of where you are currently are already?</p>
+                <p>Did you forget the last episode or chapter of an ongoing manga that you were reading and went reading another one?</p>
+                <p>You can make a list for manga and track your progress so the next time you get back to it will be easier to remember what chapter are you on.</p>
             </div>
-        </div>
+        </section>
     )
 }
 
