eb471757131a209b7f31cc53f48fea36aa85becd
Bernard Calma <nard.calma@gmail.com>
2023-03-22 19:48:41 -0700

    reroute to login if mangalist is accessed without logging in

--- src/views/MangaList.jsx
+++ src/views/MangaList.jsx
@@ -4,31 +4,42 @@ import { getMangaList } from "../features/manga/mangaSlice"
 import Manga from "../components/Manga"
 import NewManga from "../components/NewManga";
 import '../styles/mangaList.css'
+import { useNavigate } from "react-router-dom";
 
 const MangaList = () => {
     const dispatch = useDispatch(); 
+    const navigate = useNavigate()
     const {mangaList, serverURL} = useSelector(store => store.manga)
     const {user} = useSelector(store => store.user)
     useEffect(()=>{
         dispatch(getMangaList(user.id))
     },[])
     return(
-        <section className="containerMangaList">
+        <>
         {
-            mangaList.map((manga, index) => 
-                <Manga 
-                    manga = {manga}
-                    key = {manga.id}
-                    index = {index}
+            !user.id ?
+            setTimeout(() => {
+                navigate("/login")
+            })
+            :
+            <section className="containerMangaList">
+                {
+                    mangaList.map((manga, index) => 
+                        <Manga 
+                            manga = {manga}
+                            key = {manga.id}
+                            index = {index}
+                            serverURL = {serverURL}
+                        />)
+                }
+                <NewManga 
+                    mangaList = {mangaList}
                     serverURL = {serverURL}
-                />)
-        }
-        <NewManga 
-            mangaList = {mangaList}
-            serverURL = {serverURL}
-            user = {user}
-        />
-        </section>
+                    user = {user}
+                />
+            </section>
+            }
+        </>
     )
 }
 
